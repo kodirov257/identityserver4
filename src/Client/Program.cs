80703344bdcb4a69878fe9ae92f814bc3aa7d00b
Abdurahmonbek <kodirov@infosystems.uz>
2021-11-10 11:54:28 +0500

    Configured Client

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // discover endpoint from metadata
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (disco.IsError)
+            {
+                Console.WriteLine(DownloadStringCompletedEventArgs.Error);
+                return;
+            }
+            
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                
+                ClientId = "uztelecom",
+                ClientSecret = "VMx3HfE62Ju82CF",
+                Scope = "auth",
+            });
+            
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+            
+            // call api
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:7233/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
         }
     }
 }
